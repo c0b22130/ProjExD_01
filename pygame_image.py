1b934f417edbf1a524a5fbc00221db392f756603
c0b22130 <c0b22130d6@edu.teu.ac.jp>
2023-06-13 15:53:50 +0900

    練習２

---
+++ pygame_image.py
@@ -0,0 +1,27 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
+    tori_img = pg.image.load("ex01-20230613/fig/3.png")
+    tori_img = pg.transform.rotozoom(tori_img,10,1.0)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
